a102895635e0081ce3c39c678bbcbc2b3ebba74f
alexoss117 <tskf141@outlook.fr>
2026-02-01 19:53:09 +0100

    script1.ps1

--- script1.ps1
+++ script1.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri "https://github.com/alexoss117/powershell/shrek-gifgod.gif" -OutFile "C:\ST\shrek.gif"
+Invoke-WebRequest -Uri "https://c.tenor.com/b4YUypqoBmoAAAAd/tenor.gif" -OutFile "C:\ST\shrek.gif"
 
 Invoke-Item "C:\ST\shrek.gif"
